e4c11277d86cfcec5ccdbf57bb4eded6a0fcb34f
Martina Baiardi <martina.baiardi4@studio.unibo.it>
2023-09-08 16:58:04 +0200

    feat: refactor astra mas to evaluate its execution model

--- astra_concurrency/src/main/java/CustomConsole.java
+++ astra_concurrency/src/main/java/CustomConsole.java
@@ -7,4 +7,10 @@ public class CustomConsole extends Module {
         System.out.println("Hello! I'm " + super.agent.name() + " and my thread is " + Thread.currentThread().getName());
         return true;
     }
+
+    @ACTION
+    public boolean print(int value) {
+        System.out.println("The Plan " + value + " is executed on thread " + Thread.currentThread().getName());
+        return true;
+    }
 }
